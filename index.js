ee965772bc04451d3750ff71246ea3b65a8e01be
John Boi <jon0908776@gmail.com>
2022-06-15 23:32:45 -0400

    Project is finished, video needs to be recorded

--- index.js
+++ index.js
@@ -1,66 +1,177 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-
 const generateMarkdown=require("./src/generateMarkdown.js");
+
+const Intern = require ('./lib/Intern')
+const Engineer = require ('./lib/Engineer')
+const Manager = require('./lib/Manager.js');
+let wantMember='y'
+let htmElements=[];
 
 const promptUser = () => {
   return inquirer.prompt([
     {
       type: 'input',
-      name: 'projTitle',
-      message: 'What is the title of your Project?',
+      name: 'name',
+      message: "What is the name of your team's manager?",
     },
     {
       type: 'input',
-      name: 'desc',
-      message: 'Please enter the description of your project',
+      name: 'empID',
+      message: "Please enter the manager's employee ID.",
     },
     {
       type: 'input',
-      name: 'installation',
-      message: 'Please enter the installation instructions for your code',
+      name: 'email',
+      message: "Please enter the manager's email address (must include @ and .)",
     },
     {
       type: 'input',
-      name: 'usage',
-      message: 'Please enter the usage information for the project',
+      name: 'officeNum',
+      message: "Please enter the manager's office phone number (just numbers)",
     },
     {
       type: 'input',
-      name: 'contribution',
-      message: 'Let us know how to contribute to the project!',
-    },
-    {
-      type: 'input',
-      name: 'test',
-      message: 'Let us know how to test the project:',
-    },
-    {
-        type: 'input',
-        name: 'license',
-        message: 'What license would you like for the project? (Please enter one of apache, boost, bsd3, bsd2, cc0, eclipse or gnugpl3)',
-    },
-    {
-        type: 'input',
-        name: 'url',
-        message: 'Please enter your Github Username:',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Please enter your email address:',
+      name: 'addMember',
+      message: "Would you like to add another team member?",
     },
   ]);
 };
+
+const whichMember=()=>{
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'member',
+      message: "Would you like to add an Engineer or Intern? (Please type one of the choices)",
+    },
+  ]);
+}
+
+const newMember=()=>{
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'addMember',
+      message: "Would you like to add another team member? (yes/no)",
+    },
+  ]);
+}
+
+const isIntern=()=>{
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: "What is the name of your team's intern?",
+    },
+    {
+      type: 'input',
+      name: 'empID',
+      message: "Please enter the intern's employee ID.",
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: "Please enter the intern's email address (must include @ and .)",
+    },
+    {
+      type: 'input',
+      name: 'school',
+      message: "Please enter the interns school",
+    },
+  ]);
+}
+
+const isEngineer = ()=>{
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: "What is the name of your team's engineer?",
+    },
+    {
+      type: 'input',
+      name: 'empID',
+      message: "Please enter the engineer's employee ID.",
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: "Please enter the engineer's email address (must include @ and .)",
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: "Please enter the engineer's github username",
+    },
+  ]);
+}
+
+const processEmployee=()=>{
+  return whichMember()
+  .then((data2)=>{
+  if (data2.member.toLowerCase()==='intern'){
+    return isIntern()
+    .then((data3)=>{
+    wantMember=data3.addMember;
+    //Destructure and create intern
+    const {name, empID, email,school}=data3;
+    let intern= new Intern(name,parseInt(empID),email,school);
+    //Send intern to addmember fn
+    htmElements.push(intern);
+    return newMember();
+    })
+  }
+  else if (data2.member.toLowerCase()==='engineer'){
+    return isEngineer()
+    .then((data3)=>{
+      //Change While cond
+      wantMember=data3.addMember;
+      //Destructure and create intern
+      const {name, empID, email,github}=data3;
+      let engineer= new Engineer(name,parseInt(empID),email,github);
+      //Send intern to addmember fn
+      htmElements.push(engineer);
+      return newMember();
+    })
+  }
+  else{
+    console.log("You didn't enter a proper choice!")
+  }
+  })
+}
   
 
 const init = () => {
   promptUser()
     // Use writeFileSync method to use promises instead of a callback function
-    .then((answers) => fs.writeFileSync('ProfessionalReadme.md', generateMarkdown(answers)))
-    .then(() => console.log('Successfully wrote your README'))
+    .then((answers) => {
+      let mgr = new Manager(answers.name,parseInt(answers.empID),answers.email,parseInt(answers.officeNum))
+      wantMember=answers.addMember;
+      htmElements.push(mgr)
+      if(wantMember==='n'||wantMember==='no'){
+        fs.writeFileSync('./dist/index.html', generateMarkdown(htmElements))
+      }
+      else{
+        return addEmployee();
+      }
+    })
     .catch((err) => console.error(err));
 };
 
 // Function call to initialize app
+function addEmployee(){
+  return processEmployee()
+  .then((answers)=>{
+    wantMember=answers.addMember
+    if (wantMember==='n'||wantMember==='no'){
+      console.log(htmElements)
+      fs.writeFileSync('./dist/index.html', generateMarkdown(htmElements))
+    }
+    else{
+      return addEmployee();
+    }
+  })
+}
 init();
--- src/generateMarkdown.js
+++ src/generateMarkdown.js
@@ -1,5 +1,61 @@
+const Manager = require("../lib/Manager");
+const Engineer = require("../lib/Engineer");
+const Intern = require("../lib/Intern");
+let addHTML=''
+
 function generateMarkdown(data) {
-    return `HTML STUFF`
+    for (let i =1;i<data.length;i++){
+        if (data[i].getRole()==='Engineer'){
+            addHTML+=`<div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">${data[i].getName()}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">${data[i].getRole()}</h6>
+                <p class="card-text">${data[i].getId()}</p>
+                <a href="mailto: ${data[i].getEmail()}" class="card-link">${data[i].getEmail()}</a>
+                <a href = "https://github.com/${data[i].getGithub()}" class="card-link">${data[i].getGithub()}</a>
+            </div>
+        </div>`
+        }
+        else if (data[i].getRole()==='Intern'){
+            addHTML+=`<div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">${data[i].getName()}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">${data[i].getRole()}</h6>
+                <p class="card-text">${data[i].getId()}</p>
+                <a href="mailto: ${data[i].getEmail()}" class="card-link">${data[i].getEmail()}</a>
+                <p class="card-text">${data[i].getSchool()}</p>
+            </div>
+        </div>`
+        }
+    }
+
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+      <title>Team Generator</title>
+    </head>
+
+    <body>
+        <div class="jumbotron jumbotron-fluid">
+                <div class="container" id="employee-container">
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">${data[0].getName()}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">${data[0].getRole()}</h6>
+                            <p class="card-text">${data[0].getId()}</p>
+                            <a href="mailto: ${data[0].getEmail()}" class="card-link">${data[0].getEmail()}</a>
+                            <p class="card-text">${data[0].getNumber()}</p>
+                        </div>
+                    </div>
+                    ${addHTML}
+                </div>
+            </div>
+            <script src="../src/addMember.js"></script>
+        </body>
+    </html>`
 }
   
 module.exports = generateMarkdown;
